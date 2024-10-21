1c7767496bb2fb90d42764ebb4871d739ad9e712
ParsuramBaral <parsurambaral42@gmail.com>
2024-10-21 23:54:06 +0545

    Changes in Light Modes
    -Made responsive user interface, providing a delightful light mode and dark mode.
    -Made an auto transition in the image slider.

--- js/responsivenav.js
+++ js/responsivenav.js
@@ -6,6 +6,40 @@ function barclose(){
     let cancel = document.getElementsByClassName("nav-hamburger-bars")[0];
     cancel.style.visibility = "hidden";
 }
+let initialchange = true
+
+function modechange(){
+    let mode = document.querySelector("body");
+    let intromode = document.querySelectorAll('.intro')
+    let navmode = document.querySelectorAll(".nav-item");
+    let amode = document.querySelectorAll('.nav-an')
+    if (initialchange){
+        mode.style.backgroundColor = "#121212";
+        navmode.forEach(item => {
+            item.style.backgroundColor = "#121212";
+        });
+        amode.forEach(item => {
+            item.style.color = "white";
+        });
+        intromode.forEach(item =>{
+            item.style.color = "white";
+        })
+        initialchange = false;
+    }else{
+        mode.style.backgroundColor = "white";
+        navmode.forEach(item => {
+            item.style.backgroundColor = "white";
+        });
+        amode.forEach(item => {
+            item.style.color = "black";
+        });
+        intromode.forEach(item =>{
+            item.style.color = "black";
+        })
+        initialchange = true;
+    }
+    
+}
 
 function applyResponsiveBehavior() {
 
