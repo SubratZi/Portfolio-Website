62ff3c512f875d02cdc8d3f930cba84807bb98a7
ParsuramBaral <parsurambaral42@gmail.com>
2025-03-27 21:43:10 +0545

    Made Responsive
    Changed The Mode feature that works perfectly fine on every device!

--- js/modes.js
+++ js/modes.js
@@ -6,64 +6,12 @@ function barclose(){
     let cancel = document.getElementsByClassName("pages")[0];
     cancel.style.visibility = "hidden";
 }
-// let mode = document.querySelector("body");
-// let intromode = document.querySelectorAll('.intro')
-// let navmode = document.querySelectorAll(".nav-item");
-// let anmode = document.querySelectorAll('.nav-an');
-// let icon = document.querySelectorAll(".icon");
-// let description =document.querySelectorAll(".description");
-// let htmlselector = document.querySelector("html");
-
-
-
-// function modechange(){
-    
-//     if (mode.style.backgroundColor === "white" || !mode.style.backgroundColor){
-//         mode.style.backgroundColor = "#121212";
-//         navmode.forEach(item => {
-//             item.style.backgroundColor = "#121212";
-//         });
-//         anmode.forEach(item => {
-//             item.style.color = "white";
-//         });
-//         intromode.forEach(item =>{
-//             item.style.color = "white";
-//         })
-//         icon.forEach(item=>{
-//             item.style.color = "white"
-//         })
-//         description.forEach(item=>{
-//             item.style.color= "white"
-//         })
-//         localStorage.setItem("backgroundColor", "#121212");
-//         localStorage.setItem("textColor", "white")
-//     }else{
-//         mode.style.backgroundColor = "white";
-//         navmode.forEach(item => {
-//             item.style.backgroundColor = "white";
-//         });
-//         anmode.forEach(item => {
-//             item.style.color = "black";
-//         });
-//         intromode.forEach(item =>{
-//             item.style.color = "black";
-//         })
-//         icon.forEach(item=>{
-//             item.style.color = "black"
-//         })
-//         description.forEach(item=>{
-//             item.style.color= "black"
-            
-//         })
-//         localStorage.setItem("backgroundColor", "white");
-//         localStorage.setItem("textColor", "black")
-//     }
-// }
 
 let clickedbtn = document.querySelector(".light-mode");
 let htmlselector = document.querySelector("html");
+let touchbtn = document.querySelector(".responsive-mode");
 
-clickedbtn.addEventListener("click", function() {
+function modechange(){
     const currenttheme = htmlselector.getAttribute("data-theme");
     if (!currenttheme || currenttheme === "light"){
         htmlselector.setAttribute("data-theme", "dark" );
@@ -73,7 +21,10 @@ clickedbtn.addEventListener("click", function() {
         htmlselector.setAttribute("data-theme", "light" );
         savedtheme("light");
     }
-})
+}
+
+clickedbtn.addEventListener("click", modechange);
+touchbtn.addEventListener("touchstart", modechange);
 
 // Check if the system has set default dark preferences!
 
@@ -83,8 +34,6 @@ const isDarkMode = () =>{
 }
 
 window.addEventListener("load", () => {
-    // Retrieve the stored values
-    console.log("Working");
     const savedTheme = window.localStorage.getItem("theme");
     if (savedTheme){
         htmlselector.setAttribute("data-theme", savedTheme);
@@ -98,15 +47,3 @@ window.addEventListener("load", () => {
 function savedtheme(theme){
     window.localStorage.setItem("theme",theme);
 }
-
-
-//     let savedBgColor = localStorage.getItem("backgroundColor") || "white";
-//     let savedTextColor = localStorage.getItem("textColor") || "black";
-
-//     // Apply the stored values
-//     mode.style.backgroundColor = savedBgColor;
-//     navmode.forEach(item => item.style.backgroundColor = savedBgColor);
-//     anmode.forEach(item => item.style.color = savedTextColor);
-//     intromode.forEach(item => item.style.color = savedTextColor);
-//     icon.forEach(item => item.style.color = savedTextColor);
-// });
